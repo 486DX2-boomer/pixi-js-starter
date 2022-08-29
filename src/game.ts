008426e9e1e61fe3c9ba856b9bc092ecbac96f86
Danny Feller <59851021+486DX2-boomer@users.noreply.github.com>
2022-08-29 17:00:33 -0400

    Added package lock, Hello World starter

--- src/game.ts
+++ src/game.ts
@@ -8,3 +8,11 @@ const app = new PIXI.Application({
 });
 
 document.body.appendChild(app.view);
+
+const textContainer: PIXI.Container = new PIXI.Container();
+const helloWorldText: PIXI.Text = new PIXI.Text("Hello World!", {fontFamily : 'Arial', fontSize: 24, fill : 0x00000, align : 'center'});
+helloWorldText.anchor.set(0.5, 0.5);
+textContainer.x = app.screen.width / 2;
+textContainer.y = app.screen.height / 2;
+textContainer.addChild(helloWorldText);
+app.stage.addChild(textContainer);
